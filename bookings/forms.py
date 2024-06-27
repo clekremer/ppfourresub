b38fad89fb50ae0f3a1f364e7807f87edc15d660
clemens kremer <molkrich@gmx.de>
2024-06-27 17:37:50 +0000

    Add register patient logic

--- bookings/forms.py
+++ bookings/forms.py
@@ -2,9 +2,24 @@
 # bookings/forms.py
 
 from django import forms
+from django.contrib.auth.models import User
+from .models import Patient
 from .models import Appointment  # Adjusted import statement to import Appointment model
 
 class AppointmentForm(forms.ModelForm):
     class Meta:
         model = Appointment  # Using Appointment model from .models
         fields = ['patient', 'doctor', 'date', 'time', 'reason']
+
+
+class UserForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput)
+
+    class Meta:
+        model = User
+        fields = ['username', 'password', 'email', 'first_name', 'last_name']
+
+class PatientForm(forms.ModelForm):
+    class Meta:
+        model = Patient
+        fields = ['phone', 'address']
--- bookings/views.py
+++ bookings/views.py
@@ -3,6 +3,9 @@
 from django.shortcuts import render, redirect
 from .forms import AppointmentForm
 from .models import Appointment
+from django.contrib.auth.models import User
+from .forms import UserForm, PatientForm
+from .models import Patient
 
 def book_appointment(request):
     if request.method == 'POST':
@@ -17,3 +20,23 @@ def book_appointment(request):
 def appointment_list(request):
     appointments = Appointment.objects.all()
     return render(request, 'bookings/appointment_list.html', {'appointments': appointments})
+
+
+def register_patient(request):
+    if request.method == 'POST':
+        user_form = UserForm(request.POST)
+        patient_form = PatientForm(request.POST)
+        if user_form.is_valid() and patient_form.is_valid():
+            user = user_form.save(commit=False)
+            user.set_password(user_form.cleaned_data['password'])
+            user.save()
+            patient = patient_form.save(commit=False)
+            patient.user = user
+            patient.save()
+            return redirect('patient_list')
+    else:
+        user_form = UserForm()
+        patient_form = PatientForm()
+    return render(request, 'bookings/register_patient.html', {'user_form': user_form, 'patient_form': patient_form})
+
+
