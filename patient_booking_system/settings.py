c4eb362ad5c9a8eb5d612fbcca5a9f8900347201
clemens kremer <molkrich@gmx.de>
2024-09-22 19:31:27 +0000

    add pythoin runtime

--- patient_booking_system/settings.py
+++ patient_booking_system/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-)_q@2db&ty#(#w7t#nh-of#dz(=30fyohabw8p%b-rwy^hp!y$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     'localhost',
