7dc382a6f37fa58a6e9f8566d8f1bc9c770b613a
clemens kremer <molkrich@gmx.de>
2024-11-20 19:23:07 +0000

    deployment setting

--- patient_booking_system/settings.py
+++ patient_booking_system/settings.py
@@ -10,8 +10,10 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 import os
-
 from pathlib import Path
+import dj_database_url
+if os.path.isfile('env.py'):
+    import env
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -24,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-)_q@2db&ty#(#w7t#nh-of#dz(=30fyohabw8p%b-rwy^hp!y$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     '8000-clekremer-ppfourresub-ygckzpxwzps.ws-eu116.gitpod.io',
@@ -95,12 +97,17 @@ WSGI_APPLICATION = 'patient_booking_system.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#    }
+#}
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.parse(os.environ.get("DATABASE_URL", "postgres://us54i3iwius:1XSyebyE2reC@ep-gentle-mountain-a23bxz6h-pooler.eu-central-1.aws.neon.tech/boney_herbs_elder_654816"))
 }
+
 
 
 # Password validation
