ed7478ca659f636b67f0edb7470e86c98cc1a46e
clemens kremer <molkrich@gmx.de>
2024-12-21 16:55:05 +0000

    Debug .env file

--- patient_booking_system/settings.py
+++ patient_booking_system/settings.py
@@ -9,22 +9,27 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
+
+
 import os
 from pathlib import Path
 import dj_database_url
 import environ
-env = environ.Env()
-environ.Env.read_env()
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
+# Define BASE_DIR first
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+# Initialize environ
+env = environ.Env()
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))  # Explicitly load .env file
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-)_q@2db&ty#(#w7t#nh-of#dz(=30fyohabw8p%b-rwy^hp!y$'
+#SECRET_KEY = 'django-insecure-)_q@2db&ty#(#w7t#nh-of#dz(=30fyohabw8p%b-rwy^hp!y$'
+SECRET_KEY = env('SECRET_KEY', default='django-insecure-)_q@2db&ty#(#w7t#nh-of#dz(=30fyohabw8p%b-rwy^hp!y$')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
@@ -111,6 +116,7 @@ WSGI_APPLICATION = 'patient_booking_system.wsgi.application'
 DATABASES = {
     'default': dj_database_url.parse(env('DATABASE_URL'))
 }
+
 
 
 # Password validation
