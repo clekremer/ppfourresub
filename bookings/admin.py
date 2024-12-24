f85f3b412796d16d31afa12b80216da2ff58f7c8
clemens kremer <molkrich@gmx.de>
2024-12-24 06:06:58 +0000

    Code cleaning and linting in admin.py

--- bookings/admin.py
+++ bookings/admin.py
@@ -6,27 +6,32 @@ from django.utils.translation import gettext_lazy as _
 
 admin.site.register(Patient)
 admin.site.register(Doctor)
-#admin.site.register(Appointment)
-
 
 # Define an inline admin descriptor for Patient model
+
+
 class PatientInline(admin.StackedInline):
     model = Patient
     can_delete = False
     verbose_name_plural = 'patients'
 
+
 # Define an inline admin descriptor for Doctor model
+
+
 class DoctorInline(admin.StackedInline):
     model = Doctor
     can_delete = False
     verbose_name_plural = 'doctors'
 
+
 # Define a new User admin
+
+
 class UserAdmin(BaseUserAdmin):
     inlines = (PatientInline, DoctorInline)
-    
     list_display = ('username', 'email', 'is_staff', 'is_doctor', 'is_patient')
-    
+
     def is_doctor(self, obj):
         return hasattr(obj, 'doctor')
     is_doctor.boolean = True
@@ -37,7 +42,10 @@ class UserAdmin(BaseUserAdmin):
     is_patient.boolean = True
     is_patient.short_description = 'Patient'
 
+
 # Re-register UserAdmin
+
+
 admin.site.unregister(User)
 admin.site.register(User, UserAdmin)
 
@@ -45,7 +53,7 @@ admin.site.register(User, UserAdmin)
 class AppointmentAdmin(admin.ModelAdmin):
     list_display = ('patient', 'doctor', 'date', 'time', 'reason', 'status')
     list_filter = ('status', 'date', 'doctor')
-    
+
     def get_queryset(self, request):
         qs = super().get_queryset(request)
         if request.user.is_superuser:
@@ -60,5 +68,6 @@ class AppointmentAdmin(admin.ModelAdmin):
         if request.user.is_superuser:
             return self.list_display + ('doctor',)
         return self.list_display
+
 
 admin.site.register(Appointment, AppointmentAdmin)
